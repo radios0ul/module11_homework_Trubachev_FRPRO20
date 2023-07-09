e193ed0ddcd32570468cb285639db52d34714b9f
radios0ul <135992249+radios0ul@users.noreply.github.com>
2023-07-09 22:54:30 +0700

    Update 11-4.js

--- 11-4.js
+++ 11-4.js
@@ -5,12 +5,12 @@ let i = 0;
   function print() 
   { 
     if ((a + i) >= b) {clearInterval(intervalId)}
+    
     console.log(a + i);
+    
     i = i + 1;   
  }; 
-  
-const intervalId = setInterval(print, 1000);
-  
+const intervalId = setInterval(print, 1000); 
 };                  
-  
+
 main(5, 15);
